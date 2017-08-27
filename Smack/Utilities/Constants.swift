f2cae5313d69ca747241ad0422f950bd16f072a5
Your Name <avimarathe@avinashs-MacBook-Pro.local>
2017-08-27 12:37:52 +0530

    lesson-6

--- Smack/Utilities/Constants.swift
+++ Smack/Utilities/Constants.swift
@@ -8,7 +8,23 @@
 
 import Foundation
 
+//BASE URLS
+let  BASE_URL = "https://chattychatindia.herokuapp.com/v1/"
+let URL_REGISTER = "\(BASE_URL)account/register"
+
+
+//Completion Handler
+typealias completionHandler = (_ Success : Bool) -> ()
+
+//Segues
 let TO_LOGIN = "toLogin"
 let TO_CREATE_ACCOUNT = "toCreateAccount"
 let  UNWIND_TO_CHANNEL = "unwindToChannel"
+
+
+//User Defaults
+let TOKEN_KEY = "token"
+let LOGGED_IN_KEY = "loggedIn"
+let USER_EMAIL = "userEmail"
+
 
