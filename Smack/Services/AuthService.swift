3507f13e0a21e825279b9e423931e8cd53e861b1
Your Name <avimarathe@avinashs-MacBook-Pro.local>
2017-08-29 11:56:22 +0530

    lesson-8

--- Smack/Services/AuthService.swift
+++ Smack/Services/AuthService.swift
@@ -8,6 +8,7 @@
 
 import Foundation
 import Alamofire
+import SwiftyJSON
 
 class AuthService
 {
@@ -55,17 +56,13 @@ class AuthService
         
         var lowerCaseEmail = email.lowercased()
         
-        let header = [
-            "containt-Type" : "application/json; charset=utf-8"
-      ]
-        
         let body : [String:Any] = [
             "email" : lowerCaseEmail,
             "password" : password
         
         ]
         
-        Alamofire.request(URL_REGISTER, method: .post, parameters: body, encoding: JSONEncoding.default, headers: header).responseString { (responce) in
+        Alamofire.request(URL_REGISTER, method: .post, parameters: body, encoding: JSONEncoding.default, headers: HEADER).responseString { (responce) in
             if responce.result.error == nil {
                 completion(true)
             }
@@ -76,6 +73,58 @@ class AuthService
         }
         
     }
+    
+    
+    
+    func loginUser(email:String,password:String,completion:@escaping completionHandler)
+    {
+        
+        var lowerCaseEmail = email.lowercased()
+        
+        let body : [String:Any] = [
+            "email" : lowerCaseEmail,
+            "password" : password
+        ]
+        
+        Alamofire.request(URL_LOGIN, method: .post, parameters: body, encoding: JSONEncoding.default, headers: HEADER).responseJSON { (responce) in
+            if responce.result.error == nil{
+                //By general JSON way
+//                if let json = responce.result.value as? Dictionary<String,Any>{
+//                   if let email = json["user"] as? String
+//                    {
+//                        self.userEmail = email
+//                    }
+//                    if let token = json["token"] as? String
+//                    {
+//                        self.authToken = token
+//                    }
+//
+//                }
+                //Using SWIFTY JSON
+                guard let data = responce.data else {return}
+                let json = JSON(data:data)
+                self.userEmail = json["user"].stringValue
+                self.authToken = json["token"].stringValue
+                
+                self.isLonggedIn = true
+                completion(true)
+            }else
+             {
+                    completion(false)
+                    debugPrint(responce.result.error as Any)
+              }
+        }
+        
+    }
+    
+    
+    
+    
+    
+    
+    
+    
+    
     
     
     
--- Smack/Utilities/Constants.swift
+++ Smack/Utilities/Constants.swift
@@ -11,7 +11,7 @@ import Foundation
 //BASE URLS
 let  BASE_URL = "https://chattychatindia.herokuapp.com/v1/"
 let URL_REGISTER = "\(BASE_URL)account/register"
-
+let URL_LOGIN = "\(BASE_URL)account/login"
 
 //Completion Handler
 typealias completionHandler = (_ Success : Bool) -> ()
@@ -26,5 +26,21 @@ let  UNWIND_TO_CHANNEL = "unwindToChannel"
 let TOKEN_KEY = "token"
 let LOGGED_IN_KEY = "loggedIn"
 let USER_EMAIL = "userEmail"
+
+
+//Header
+
+let HEADER =  [
+    "containt-Type" : "application/json; charset=utf-8"
+]
+
+
+
+
+
+
+
+
+
 
 
